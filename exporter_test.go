ab0d01d3fca87a0353377c4cd9cdde78ae70302c
Guo Y.K <hi@guoyk.net>
2020-06-09 16:38:23 +0800

    Update exporter_test.go
    fxxk t.Logf

--- exporter_test.go
+++ exporter_test.go
@@ -3,6 +3,7 @@ package esexporter
 import (
 	"context"
 	"github.com/guoyk93/logutil"
+	"log"
 	"os"
 	"strconv"
 	"testing"
@@ -13,7 +14,7 @@ func TestExporter_Do(t *testing.T) {
 	batch, _ := strconv.ParseInt(os.Getenv("ES_BATCH"), 10, 64)
 	var totalCount, totalSize, maxId int64
 
-	prg := logutil.NewProgress(logutil.LoggerFunc(t.Logf), "test")
+	prg := logutil.NewProgress(logutil.LoggerFunc(log.Printf), "test")
 
 	handler := DocumentHandler(func(buf []byte, id int64, total int64) error {
 		if max > 0 && id >= max {
@@ -37,14 +38,14 @@ func TestExporter_Do(t *testing.T) {
 			},
 		},
 		Batch:       batch,
-		DebugLogger: t.Logf,
+		DebugLogger: log.Printf,
 	}, handler)
 
 	if err := e.Do(context.Background()); err != nil {
 		t.Fatal(err)
 	}
 
-	t.Logf("max id: %d", maxId)
-	t.Logf("total count: %d", totalCount)
-	t.Logf("total raw size: %dmb", totalSize/1024/1024)
+	log.Printf("max id: %d", maxId)
+	log.Printf("total count: %d", totalCount)
+	log.Printf("total raw size: %dmb", totalSize/1024/1024)
 }
